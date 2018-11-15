c7b221c3be6cffb5e8ddac4536f4a2d7c049e615
Sergii Piatakov <sergii.piatakov@globallogic.com>
2018-11-15 15:26:35 +0200

    formatting: use tabs instead of spaces
    Signed-off-by: Sergii Piatakov <sergii.piatakov@globallogic.com>

--- calculator.cpp
+++ calculator.cpp
@@ -2,10 +2,20 @@
 
 int Calculator::Add (int a, int b)
 {
+<<<<<<< HEAD
     return a + b;
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+	return a + b + 0.5;
+>>>>>>> 4ad40a3 (fix truncation error)
+=======
+	return a + b;
+>>>>>>> cbcb06d (formatting: use tabs instead of spaces)
+>>>>>>> 4ac167f (formatting: use tabs instead of spaces)
 }
 
 int Calculator::Sub (int a, int b)
 {
-    return Add (a, -b);
+	return Add (a, -b);
 }
--- calculator.h
+++ calculator.h
@@ -3,9 +3,15 @@
 
 class Calculator
 {
+<<<<<<< HEAD
     public:
         int Add (int, int);
         int Sub (int, int);
+=======
+	public:
+		int Add (double, double);
+		int Sub (double, double);
+>>>>>>> 4ac167f (formatting: use tabs instead of spaces)
 };
 
 #endif//CALCULATOR_H
